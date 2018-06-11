4b37e91235675ac4514504a19a23323c13767f0a
Jeremy Parker <jrobinparker@gmail.com>
2018-06-11 17:25:25 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 array1 = ["kiwi"]
 array2 = ["kiwi", "durian"]
 array3 = ["kiwi", "durian", "starfruit", "mangos", "dragon fruits"]
@@ -12,3 +13,11 @@ def oxford_comma(array)
     return array.join(", ") + ", and " + last_element
   end
 end
+=======
+def oxford_comma(array)
+  array1 = ["kiwi"]
+  return array1.join
+  array2 = ["kiwi", "durian"]
+  return array2.join(" and ")
+end
+>>>>>>> 6470c60ac1b926c82b5f74e80893c005f1ebed49
